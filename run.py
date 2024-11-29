011a328b43cc343ff720c4968eea457ba06f2e94
Yadamzer Terefe <140100340+yadi09@users.noreply.github.com>
2024-11-30 02:40:06 +0300

    Update run.py

--- run.py
+++ run.py
@@ -9,3 +9,5 @@ from app.models import User, Student, Laptop, LaptopImage
 def make_shell_context():
     # Expose these objects in the Flask shell for easy access
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Student': Student, 'Laptop': Laptop, 'LaptopImage': LaptopImage}
+
+
