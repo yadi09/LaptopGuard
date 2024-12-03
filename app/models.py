2a4215caa35eb70ae321a52d50d995eaf6e53903
Yadamzer Terefe <140100340+yadi09@users.noreply.github.com>
2024-12-04 00:34:55 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -176,9 +176,3 @@ class ExitLogs(db.Model):
     student: so.Mapped[Student] = so.relationship(
         'Student',
         back_populates='exit_logs')  # Relationship to the Student model
-
-
-
-
-
-
